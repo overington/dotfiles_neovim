6729f165c38519df6b471f8ac017c4b0f583f005
Samuel Overington <soverington@expediagroup.com>
2023-01-31 14:26:09 +0000

    change colorscheme + small config changes

--- ftplugin/python_mappings.vim
+++ ftplugin/python_mappings.vim
@@ -1,61 +1,14 @@
 let g:python_config = "~/.config/nvim/ftplugin/python_mappings.vim"
 
 " syntax --- {{{
-set textwidth=88
-set colorcolumn=88,110
+" set textwidth=88
+" set colorcolumn=88,110
 " }}}
 
 nnoremap <leader>efp :exe ':vsp 'g:python_config<cr>
-" nnoremap <leader>vp :python.venvPath = $WORKON_HOME + "/bin/python"
-nnoremap <leader>vp :python.venvPath = %{$VIRTUAL_ENV} . "/bin/python"
-nnoremap <buffer> H :<C-u>execute "!pydoc3 " . expand("<cword>")<CR>
 
-" Select in python method - eg np.ar*ray() will select [np.array]()
-" Needs a little bit more work - as if there is no '()' at the end of the end,
-" it will not work
-" select all in current method
-nnoremap vim t(m"T v`"
-" nnoremap <S-;> <S-V>""y<CR> :!pydoc3 <C-r>0<CR>
+nnoremap <buffer> H :<C-u>execute "!pydoc3 " . expand("<cword>")<CR>
 nnoremap <C-j> :!pydoc3 <cword><CR>
 " Execute script
 vnoremap <leader>xc ""y :python3 <C-r>0<CR>
 " vnoremap 
-
-
-" vnoremap <NL> :python %<CR>
-" nnoremap <NL> :!python %<CR>
-nnoremap <leader>xp :w<CR>:!python %<CR>
-" inoremap <C-X> <ESC>:w<CR>:!python %<CR>
-
-
-let g:pyindent_open_paren = '&sw * 2'  " Default values
-" set tabstop=2 shiftwidth=2 expandtab
-" set list
-" set lcs=tab:>-,trail:-
-
-" nmap <silent> <leader>+ :call ToggleIndentGuidesTabs()<cr>
-" nmap <silent> <leader>= :call ToggleIndentGuidesSpaces()<cr>
-
-" function! ToggleIndentGuidesTabs()
-  " if exists('b:iguides_tabs')
-		" setlocal nolist
-		" let &l:listchars = b:iguides_tabs
-		" unlet b:iguides_tabs
-	" else
-		" let b:iguides_tabs = &l:listchars
-		" setlocal listchars=tab:┆\ "protect the space
-		" setlocal list
-	" endif
-" endfunction
-
-" function! ToggleIndentGuidesSpaces()
-	" if exists('b:iguides_spaces')
-		" call matchdelete(b:iguides_spaces)
-		" unlet b:iguides_spaces
-	" else
-		" let pos = range(1, &l:textwidth, &l:shiftwidth)
-		" call map(pos, '"\\%" . v:val . "v"')
-		" let pat = '\%(\_^\s*\)\@<=\%(' . join(pos, '\|') . '\)\s'
-		" let b:iguides_spaces = matchadd('CursorLine', pat)
-	" endif
-" endfunction
--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
--- Install packer
+-- https://github.com/nvim-lua/kickstart.nvim
+-- install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
+local HOME = os.getenv('HOME')
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   is_bootstrap = true
   vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
@@ -48,7 +50,8 @@ require('packer').startup(function(use)
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
 
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
+  -- use 'navarasu/onedark.nvim' -- Theme inspired by Atom
+  use 'junegunn/seoul256.vim'
   use 'junegunn/goyo.vim' -- Distraction free writing
   use 'junegunn/limelight.vim' -- Focus on the lin you are writing, and dim prev / post
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
@@ -120,8 +123,8 @@ vim.o.breakindent = true
 vim.o.undofile = true
 
 -- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.o.ignorecase = false
+vim.o.smartcase = false
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -129,21 +132,39 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme onedark]]
+vim.g.seoul256_background = 235
+vim.cmd [[colorscheme seoul256]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
 -- [[ Basic Keymaps ]]
--- Set <space> as the leader key
+-- Set <,> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ','
 vim.g.maplocalleader = '`'
 
+-- Set host progs
+-- python
+vim.g.python3_host_prog = HOME .. '/.config/nvim/venv/bin/python3'
+vim.g.python_host_prog = HOME .. '/.config/nvim/venv/bin/python'
+-- node
+local NODE_VERSION_FILE = io.open(HOME .. '/.nvm/alias/default')
+if NODE_VERSION_FILE then
+  local NODE_VERSION = NODE_VERSION_FILE:read()
+  local NODE_PATH = HOME .. '/.nvm/versions/node/v'.. NODE_VERSION ..'/bin/node'
+  -- print("Setting node path: " .. NODE_PATH)
+  vim.g.node_host_prog = NODE_PATH
+  NODE_VERSION_FILE:close()
+end
+
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set('n', '<leader>ei', ':e $HOME/.config/nvim/init.lua<CR>', {desc = '[E]dit [I]nit.lua'})
+vim.keymap.set('n', '<leader><leader>ei', ':tabe $HOME/.config/nvim/init.lua<CR>', {desc = '[E]dit [I]nit.lua in new tab'})
+vim.keymap.set('n', '<leader>si', ':source $HOME/.config/nvim/init.lua<CR>', {desc = '[S]ource [I]nit.lua'})
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -165,7 +186,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'onedark',
+    theme = 'seoul256',
     component_separators = '|',
     section_separators = '',
   },
@@ -333,6 +354,7 @@ local on_attach = function(_, bufnr)
   nmap('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
+  -- nmap('<leader>gu', vim.api.nvim_command(':e %:h<CR>'), '[G]o [U]p directory')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
